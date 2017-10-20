31db3483c973ac4fce350e2076f58feabd3b28c8
fercon997 <fercon997@gmail.com>
2017-10-20 19:54:29 -0400

    Added new projects page

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ app.get("/about", (req,res) => {
     pageTitle: "About page"
   });
 });
+app.get("/projects", (req,res) =>{
+  res.render('projects.hbs', {
+    pageTitle: "Projects"
+  });
+});
 app.get("/bad", (req,res) => {
   res.send({
     errorMessage: "Something went wrong"
